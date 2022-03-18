f512578fd30f690adfc218526b6f31dbdb5c5f42
Katrina Lenell Hill <khill365@gmail.com>
2022-03-17 21:37:59 -0700

    added code to solve challenge 2

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -40,6 +40,7 @@ For example: 'Cat' would come before 'apple'
 
 const sortNames = (arr) => {
   // Solution code here...
+  return arr.sort();
 };
 
 /* ------------------------------------------------------------------------------------------------
