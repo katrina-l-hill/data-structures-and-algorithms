65bbafb502c7df32dd0083fc0ad5be677018d8d2
Katrina Lenell Hill <khill365@gmail.com>
2022-03-21 23:26:36 -0700

    added code to solve challenges 3 and 4

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -40,6 +40,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  return str.concat(' The end.');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -57,7 +58,8 @@ console.log(a) prints [1, 2, 3, 1]
 
 const appendFirstToLast = (arr) => {
   // Solution code here...
-};
+  arr.push(arr[0]);
+  };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -76,6 +78,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
