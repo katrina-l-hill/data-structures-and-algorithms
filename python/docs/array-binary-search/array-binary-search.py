752ae20dca8b1998c8daf8bf904506aa5e700d7e
Katrina Hill <khill365@gmail.com>
2022-04-20 17:12:13 -0700

    edited function, added print functions

--- python/docs/array-binary-search/array-binary-search.py
+++ python/docs/array-binary-search/array-binary-search.py
@@ -2,12 +2,12 @@ import math
 
 def BinarySearch (arr, search_term):
     begin = 0
-    end = arr.length - 1
+    end = len(arr) - 1
     while begin <= end:
-        middle = math.floor(begin + end)
-        if arr(middle) < search_term:
+        middle = math.floor(begin + end) // 2
+        if arr[middle] < search_term:
             begin = middle + 1
-        elif arr(middle) > search_term:
+        elif arr[middle] > search_term:
             end = middle - 1
         else:
             return middle
@@ -15,4 +15,8 @@ def BinarySearch (arr, search_term):
     return -1
     print('The element is not in the array')
 
+print(BinarySearch([4, 8, 15, 16, 23, 42], 15))
+print(BinarySearch([-131, -82, 0, 27, 42, 68, 179], 42))
+print(BinarySearch([11, 22, 33, 44, 55, 66, 77], 90))
+print(BinarySearch([1, 2, 3, 5, 6, 7], 4))
 
