b4594ab6be8f58ba42804ae8c60da40ac80ce1c4
Katrina Hill <khill365@gmail.com>
2022-04-22 21:52:13 -0700

    added while loop for current and target values

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -11,6 +11,18 @@ class LinkedList:
     def insert(self, value):
         self.head = Node(value, self.head)
 
+    # value is the target value we're looking for to see if it's included in this list or not
+    def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+                # If we find the value we're looking for, it will return True and the while loop will exit as well as the function
+
+            current = current.next
+            # If current gets to the end after current.next then the value will be None and the function will go down to return False
+        return False
+
 
 class Node:
     def __init__(self, value, next=None):
