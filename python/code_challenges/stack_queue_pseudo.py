755cb08ac33952c5aa18e63f3558329c251f69a1
Katrina Hill <khill365@gmail.com>
2022-05-02 18:41:38 -0700

    added base code with initializer and methods for enqueue and dequeue; passed test_enqueue_one

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,28 @@
 from data_structures.stack import Stack
+from data_structures.linked_list import Node
 
 
 class PseudoQueue:
-    pass
+    def __init__(self, stack_1, stack_2):
+        self.stack_1 = stack_1
+        self.stack_2 = stack_2
+
+    def enqueue(self, value):
+        self.stack_1.push(value)
+
+    def dequeue(self):
+        # temp = self.stack_1.pop()
+        # self.stack_2.push(temp)
+        return_value = None
+        value = self.stack_1.pop()
+        while value is not None:
+            if self.stack_1.peek() != None:
+                self.stack_2(value)
+            else:
+                return_value = self.stack_1.peek()
+            value = self.stack_1.pop()
+        value = self.stack_2.pop()
+        while value is not None:
+            self.stack_1.push(value)
+            value = self.stack_2.pop()
+        return return_value
