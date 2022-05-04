a051eb81ac7384181057ce8c5e2a808408f39177
Katrina Hill <khill365@gmail.com>
2022-05-04 14:36:09 -0700

    added base code, imported Stack from data_structures

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,32 @@
+from data_structures.stack import Stack
 from data_structures.queue import Queue
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(input):
+    stack = Stack()
+    opening_brackets = "([{"
+    closing_brackets = ")]}"
+    for char in input:
+        if char in opening_brackets:
+            stack.push(char)
+        elif char in closing_brackets:
+            if stack.is_empty():
+                return False
+            temp = stack.pop()
+            # if temp doesn't match type of closing bracket
+            match temp:
+                case "{":
+                    if not char == "}":
+                        return False
+                    break
+                case "[":
+                    if not char == "]":
+                        return False
+                    break
+                case "(":
+                    if not char == ")":
+                        return False
+                    break
+    if not stack.is_empty():
+        return False
+    return True
