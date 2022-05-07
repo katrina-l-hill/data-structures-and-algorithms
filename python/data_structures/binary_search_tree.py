61e34aef92570d519b7469a6877756ea8729a4d6
Katrina Hill <khill365@gmail.com>
2022-05-06 21:31:18 -0700

    added new data from lecture in production files and tests

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,15 +1,34 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
-    """
-    Put docstring here
-    """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def add(self, value):
 
-    def some_method(self):
-        # method body here
-        pass
+        def walk(root, new_node):
+
+            # base case
+            if not root:
+                return
+
+            if new_node.value < root.value:
+                # go left
+                if root.left:
+                    walk(root.left, new_node)
+                else:
+                    root.left = new_node
+            else:
+                # go right
+                if root.right:
+                    walk(root.right, new_node)
+                else:
+                    root.right = new_node
+
+        node_to_add = Node(value)
+
+        if not self.root:
+            self.root = node_to_add
+            return
+
+        walk(self.root, node_to_add)
+
