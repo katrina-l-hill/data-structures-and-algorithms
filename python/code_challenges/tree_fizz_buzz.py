115a1875b6af50716c7c3455d2f13b1c328908aa
Katrina Hill <khill365@gmail.com>
2022-05-11 21:26:09 -0700

    added base code for fizz_buzz_tree function

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -2,4 +2,12 @@ from data_structures.binary_tree import BinaryTree
 
 
 def fizz_buzz_tree():
-    pass
+    for item in range:
+        if item % 15 == 0:
+            print("FizzBuzz")
+        if item % 3 == 0:
+            print("Fizz")
+        if item % 5 == 0:
+            print("Buzz")
+        else:
+            print(item)
