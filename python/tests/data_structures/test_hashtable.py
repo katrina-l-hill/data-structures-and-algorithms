c9033dbb24aae37d444a655ea67b7e12566e9119
Katrina Hill <khill365@gmail.com>
2022-05-31 13:43:13 -0700

    added contains, keys, and revised hash methods

--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -1,3 +1,4 @@
+from cmath import exp
 import pytest
 from data_structures.hashtable import Hashtable
 
@@ -6,12 +7,21 @@ def test_exists():
     assert Hashtable
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_get_apple():
     hashtable = Hashtable()
     hashtable.set("apple", "Used for apple sauce")
     actual = hashtable.get("apple")
     expected = "Used for apple sauce"
+    assert actual == expected
+
+
+# @pytest.mark.skip("TODO")
+def test_missing_key():
+    hashtable = Hashtable()
+    hashtable.set("apple", "Used for apple sauce")
+    actual = hashtable.get("APPLE")
+    expected = None
     assert actual == expected
 
 
@@ -25,10 +35,46 @@ def test_internals():
     actual = []
 
     # NOTE: purposely breaking encapsulation to test the "internals" of Hashmap
-    for item in hashtable._buckets:
+    for item in hashtable.buckets:
         if item:
             actual.append(item.display())
 
     expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
 
+    assert actual == expected
+
+
+# @pytest.mark.skip("TODO")
+def test_keys():
+    hashtable = Hashtable(1024)
+    hashtable.set("ahmad", 30)
+    hashtable.set("silent", True)
+    hashtable.set("listen", "to me")
+    actual = list(hashtable.keys())
+    actual.sort()
+    expected = ["ahmad", "listen", "silent"]
+    assert actual == expected
+
+
+# @pytest.mark.skip("TODO")
+def test_hash():
+    hashtable = Hashtable(1024)
+    hash1 = hashtable.hash("ahmad")
+    hash2 = hashtable.hash("hamda")
+    hash3 = hashtable.hash("damha")
+    assert hash1 == hash2 and hash2 == hash3
+
+
+# @pytest.mark.skip("TODO")
+def test_collisions():
+    hashtable = Hashtable(1024)
+    hashtable.set("ahmad", 30)
+    hashtable.set("hamda", 40)
+    hashtable.set("damha", 50)
+    keyset = list(hashtable.keys())
+    keyset.sort()
+    actual = []
+    for key in keyset:
+        actual.append((key, hashtable.get(key)))
+    expected = [("ahmad", 30), ("damha", 50), ("hamda", 40)]
     assert actual == expected
