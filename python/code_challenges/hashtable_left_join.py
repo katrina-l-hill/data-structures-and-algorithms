6be665c07ae2c65ecb096ef1a25f621b7206dde4
Katrina Hill <khill365@gmail.com>
2022-06-01 23:39:37 -0700

    added left_join function

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,18 @@
-from data_structures.hashtable import Hashtable
-
-
-def left_join():
-    pass
+def left_join(hashmap_1, hashmap_2):
+    output = (
+        []
+    )  # empty list (new data structure) for the results of the joined hashmaps
+    for (
+        key
+    ) in (
+        hashmap_1
+    ):  # start with the first hashmap since all it's values will be returned.
+        left_side_first = [
+            key,
+            hashmap_1.get(key),
+            hashmap_2.get(key),
+        ]  # the keys are retrieved and assigned to this variable
+        output.append(
+            left_side_first
+        )  # values on the right side (hashmap_2) will be appended to the left side (hashmap_1)
+    return output
